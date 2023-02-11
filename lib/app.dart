a217111b09739b6e6b42bd3d1143ec7d2a2451aa
fen0268 <Kenshi150107@gmail.com>
2023-02-11 23:18:57 +0900

    no message

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animation_sample_app/home_page.dart';
+import 'package:flutter_animation_sample_app/pages/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
