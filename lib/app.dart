facd1eb888db2a4caf7fdcee86ba6f4b3133daa7
fen0268 <Kenshi150107@gmail.com>
2023-02-22 19:18:49 +0900

    no message

--- lib/app.dart
+++ lib/app.dart
@@ -6,8 +6,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      home: const HomePage(),
     );
   }
 }
