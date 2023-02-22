795e77994b1e0157942ce5ed6699b29f5d3e0c63
fen0268 <Kenshi150107@gmail.com>
2023-02-22 19:19:39 +0900

    単位ベクトルを

--- lib/utils/vector.dart
+++ lib/utils/vector.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 Offset vectors(int num) {
   if (num == 0) {
-    return const Offset(1, -1);
+    return const Offset(4, -4);
   } else if (num == 1) {
-    return const Offset(-1, -1);
+    return const Offset(-4, -4);
   } else if (num == 2) {
-    return const Offset(-1, 1);
+    return const Offset(-4, 4);
   } else if (num == 3) {
-    return const Offset(1, 1);
+    return const Offset(4, 4);
   } else {
     return const Offset(0, 0);
   }
